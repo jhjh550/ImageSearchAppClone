b51457770e05f823a44c9cf217b1a048e332b573
jh-4750 <navith@naver.com>
2020-09-25 16:46:31 +0900

    - add pagingSource & pagingData

--- app/src/main/java/com/example/imagesearchappclone/data/UnsplashRepository.kt
+++ app/src/main/java/com/example/imagesearchappclone/data/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.example.imagesearchappclone.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.example.imagesearchappclone.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -7,4 +10,9 @@ import javax.inject.Singleton
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
 
+    fun getSearchResults(query:String) =
+        Pager(
+            config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query)}
+        ).liveData
 }
--- app/src/main/java/com/example/imagesearchappclone/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/imagesearchappclone/ui/gallery/GalleryViewModel.kt
@@ -1,11 +1,28 @@
 package com.example.imagesearchappclone.ui.gallery
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.example.imagesearchappclone.data.UnsplashRepository
 import javax.inject.Inject
 
 class GalleryViewModel @ViewModelInject constructor (
     private val repository: UnsplashRepository
 ): ViewModel() {
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { queryString ->
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query:String){
+        currentQuery.value = query
+    }
+
+    companion object{
+        private const val DEFAULT_QUERY = "cats"
+    }
 }
